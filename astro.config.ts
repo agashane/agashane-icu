c8af63f5a91533b54fabec70d302c7e373042201
Antoine-Glorieux Lukama <ir.antoineglorieux@gmail.com>
2026-03-26 07:45:46 +0200

    feat: add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 import { remarkReadingTime } from "./src/lib/reading-time";
+
+const isDev = import.meta.env.MODE === "development";
 
 // https://astro.build/config
 export default defineConfig({
+  site: isDev ? "http://localhost:4321" : "https://agashane.icu",
   adapter: cloudflare(),
   fonts: [
     {
@@ -27,5 +31,19 @@ export default defineConfig({
   },
   markdown: {
     remarkPlugins: [remarkReadingTime],
+  },
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-GB",
+          fr: "fr-FR",
+        },
+      },
+    }),
+  ],
+  devToolbar: {
+    enabled: isDev,
   },
 });
